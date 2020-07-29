2bdae2fc71760566efbd5ab3f967321163617ca1
salman-abedin <salman104alvi@gmail.com>
2020-07-29 09:43:37 +0600

    Added support files with spaces in alias mode

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ git clone https://github.com/salman-abedin/devour.git && cd devour && sudo make 
 
 ```sh
 devour CMD/ALIAS ...
-devour CMD/ALIAS ... -- [FILE with spaces]
+devour CMD/ALIAS ... -- [SAFE FILE/UNSAFE FILE]
 ```
 
 ## Pro Tip
--- devour.sh
+++ devour.sh
@@ -3,7 +3,7 @@
 # Hides your terminal window before launching an external program and unhides it after quitting.
 # Dependencies: sed, xdo
 # Usage: devour CMD/ALIAS ...
-#        devour CMD/ALIAS ... -- [FILE with spaces]
+#        devour CMD/ALIAS ... -- [SAFE FILE/UNSAFE FILE]
 
 ARGS=$*
 
