b63f5924180869bfcc89bbc750bc84767e4b1450
salman-abedin <salman104alvi@gmail.com>
2020-07-30 12:46:23 +0600

    Added support files with spaces in alias mode

--- devour.sh
+++ devour.sh
@@ -3,7 +3,7 @@
 # Hides your terminal window before launching an external program and unhides it after quitting.
 # Dependencies: sed, xdo
 # Usage: devour CMD/ALIAS ... SAFE FILE ...
-#        devour CMD/ALIAS ... -- SAFE FILE / UNSAFE FILE
+#        devour CMD/ALIAS ... -- SAFE FILE/UNSAFE FILE
 
 ARGS=$*
 CMD="${ARGS%% -- *}"
