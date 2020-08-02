e4dcc2c843f050e4189eca1c46d2ef1ec9c197fc
salman-abedin <salman104alvi@gmail.com>
2020-08-03 03:10:50 +0600

    Ported the support for files with spaces

--- devour.c
+++ devour.c
@@ -3,30 +3,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *str;
+
+void fixpath(int argc, char *argv[], int i) {
+  while (i < argc) {
+    strcat(str, argv[i]);
+    if (i != argc - 1)
+      strcat(str, "\\ ");
+    else
+      strcat(str, " ");
+    ++i;
+  }
+}
+
 void runcommand(int argc, char *argv[]) {
   int arglen = 1;
-  char *str, *head = "$SHELL -i -c \"", *tail = " > /dev/null 2>&1; exit\"";
+  char *head = "$SHELL -i -c \"", *tail = "> /dev/null 2>&1; exit\"";
 
-  arglen += strlen(head);
+  arglen += strlen(head) + strlen(tail);
   for (int i = 1; i < argc; ++i)
-    arglen += 1 + strlen(argv[i]);
-  arglen += strlen(tail);
+    arglen += 2 + strlen(argv[i]);
   str = (char *)malloc(sizeof(char) * arglen);
 
   strcpy(str, head);
   for (int i = 1; i < argc; ++i) {
-    if (!strcmp(argv[i], "--"))
-      goto procfile;
+    if (!strcmp(argv[i], "--")) {
+      fixpath(argc, argv, ++i);
+      break;
+    }
     strcat(str, argv[i]);
     strcat(str, " ");
   }
-procfile:
-  strcat(str, "file");
-
   strcat(str, tail);
 
-  printf("%s", str);
-  /* system(str); */
+  /* printf("%s", str); */
+  system(str);
   free(str);
 }
 
