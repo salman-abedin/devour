e5d83f94650a83ab1f57529f46df6564e5380b90
salman-abedin <salman104alvi@gmail.com>
2020-08-22 03:03:02 +0600

    Ported the support for files with spaces

--- devour.c
+++ devour.c
@@ -7,26 +7,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-void _fix_path(char** argv, char* upath) {
-   while (*++argv) {
-      strcat(upath, *argv);
-      if (*(argv + 1) != 0) strcat(upath, "\\ ");
-   }
-}
-
 void run_command(char** argv) {
+   int is_unsafe = 0;
    char cmd[512] = {0};
 
    strcat(cmd, "$SHELL -i -c \"");
    while (*++argv) {
-      if (strcmp(*argv, "--") == 0) {
-         _fix_path(argv, cmd);
-         break;
+      if (!strcmp(*argv, "--")) {
+         is_unsafe = 1;
+         continue;
       }
       strcat(cmd, *argv);
-      strcat(cmd, " ");
+      strcat(cmd, is_unsafe && *(argv + 1) ? "\\ " : " ");
    }
-   strcat(cmd, " > /dev/null 2>&1; exit\"");
+   strcat(cmd, "> /dev/null 2>&1; exit\"");
    system(cmd);
 }
 
