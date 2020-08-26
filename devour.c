993b15586db2f25cab303b5abc4d429935cbe597
salman-abedin <salman104alvi@gmail.com>
2020-08-27 00:47:25 +0600

    messing around

--- devour.c
+++ devour.c
@@ -8,28 +8,31 @@
 #include <string.h>
 
 void run_command(char** argv) {
-   int is_unsafe = 0;
+   int is_safe = 1;
    char cmd[512] = {0};
 
+   /* strcat(cmd, "echo "); */
    while (*++argv) {
       if (!strcmp(*argv, "--")) {
-         is_unsafe = 1;
+         is_safe = 0;
          continue;
       }
-      if (strstr(*argv, " ")) {
+      if (is_safe) {
+         strcat(cmd, *argv);
+      } else {
          int i = 0;
          char* str = *argv;
          while (str[i]) {
             if (str[i] == ' ') {
-               strcat(cmd, "\\ ");
-               ++i;
-            } else
-               strncat(cmd, &str[i++], 1);
+               strncat(cmd, "\\", 1);
+            }
+            strncat(cmd, &str[i], 1);
+            ++i;
          }
-      } else
-         strcat(cmd, *argv);
-      strcat(cmd, is_unsafe && *(argv + 1) ? "\\ " : " ");
+      }
+      strcat(cmd, " ");
    }
+   /* strcat(cmd, "> ~/devour"); */
    system(cmd);
 }
 
@@ -38,11 +41,11 @@ int main(int argc, char** argv) {
    Window win;
    Display* dis = XOpenDisplay(0);
 
-   XGetInputFocus(dis, &win, &rev);
-   XUnmapWindow(dis, win);
-   XFlush(dis);
+   /* XGetInputFocus(dis, &win, &rev); */
+   /* XUnmapWindow(dis, win); */
+   /* XFlush(dis); */
    run_command(argv);
-   XMapWindow(dis, win);
-   XCloseDisplay(dis);
+   /* XMapWindow(dis, win); */
+   /* XCloseDisplay(dis); */
    return 0;
 }
